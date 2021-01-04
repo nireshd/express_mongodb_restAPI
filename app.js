ee3232f6efc5b53d87b87b2e01ddffcff0d6a917
nireshd <niresh_dravin@live.com>
2021-01-04 13:26:14 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
 const topicController = require("./controllers/TopicController");
-
 
 // db instance connection
 require("./config/db");
 
 const app = express();
-
 const port = process.env.PORT || 8001;
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// API ENDPOINTS
-
+// API endpoint
 app
     .route("/search")
     .get(topicController.readTopics)
 
-
+// Serve
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
