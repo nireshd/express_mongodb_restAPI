fbeba35e3a63a02d253f9567a00ce6bdb8a5cea8
nireshd <niresh_dravin@live.com>
2021-01-16 04:51:00 +0800

    Update TopicController.js

--- controllers/TopicController.js
+++ controllers/TopicController.js
@@ -1,15 +1,30 @@
 const Topic = require("../models/Topic");
+const Annotation = require("../models/Annotation");
 
 exports.readTopics = (req, res) => {
     console.log(req.query.q);
-    Topic.find({ "topic": req.query.q }, (err, result) => {
+    Topic.distinct("topic",
+        {
+            "$or":
+                [{ "ancestors": req.query.q }, { "topic": req.query.q }]
+        },
+        (err, result) => {
         if (err) {
             res.status(500).send(err);
         }
-        try { res.status(200).json(result[0]["question_numbers"]); }
-        catch (err) { res.status(200).json({"Empty String Returned":"No results found"})}
+        try {
+            Annotation.distinct("question_numbers",
+                { "annotation": result },
+                (err, result2) => {
+                if (err) {
+                    res.status(500).send(err);
+                }
+                try {
+                    res.status(200).json(result2); 
+                }
+                catch (err) { res.status(200).json({ "Empty Array Returned": "No Questions Stored" }) }
+            });
+        }
+        catch (err) { res.status(200).json({"Empty String Returned":"Search String unmatched"})}
     });
 };
-
-
-
