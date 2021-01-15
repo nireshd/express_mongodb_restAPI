0f4c4a8ff5da7e978a877323e52c458ec156daa2
nireshd <niresh_dravin@live.com>
2021-01-16 04:52:11 +0800

    Update Topic.js

--- models/Topic.js
+++ models/Topic.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-
-
 const TopicsSchema = new Schema({
     topic: {
         type: String,
@@ -11,7 +9,6 @@ const TopicsSchema = new Schema({
     _id: [{
         type: String
     }],
-
 });
 
 module.exports = mongoose.model("Topic", TopicsSchema);
